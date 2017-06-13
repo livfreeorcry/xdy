cc993fb5e14b2ccb66c5438f4cbfba2a6cde9000
rasputine <james@armifi.com>
2017-06-12 20:11:06 -0700

    adding initial py file with flask

---
+++ slackroller.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def roller():
+	rollString=request.args.get('roll')
+	return roll
